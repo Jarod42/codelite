e58955336b07f77e46a22a8a919f9d9d0446f295
Jarod42 <Jarod42@users.noreply.github.com>
2024-03-26 22:46:24 +0100

    WorkAround for msvc: * flex/yacc generated code issue: - remove `#include <unistd.h>` * wxrc issues with empty files.

--- Subversion2/subversion2_subversion2_bitmaps.cpp
+++ Subversion2/subversion2_subversion2_bitmaps.cpp
@@ -22,20 +22,16 @@
 #endif
 
 static size_t xml_res_size_0 = 0;
-static unsigned char xml_res_file_0[] = {
-};
+static unsigned char* xml_res_file_0 = nullptr;
 
 static size_t xml_res_size_1 = 0;
-static unsigned char xml_res_file_1[] = {
-};
+static unsigned char* xml_res_file_1 = nullptr;
 
 static size_t xml_res_size_2 = 0;
-static unsigned char xml_res_file_2[] = {
-};
+static unsigned char* xml_res_file_2 = nullptr;
 
 static size_t xml_res_size_3 = 0;
-static unsigned char xml_res_file_3[] = {
-};
+static unsigned char* xml_res_file_3 = nullptr;
 
 static size_t xml_res_size_4 = 585;
 static unsigned char xml_res_file_4[] = {
